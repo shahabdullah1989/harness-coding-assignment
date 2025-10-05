d62efd4a786fcd7b6210360e9f857d7b76c48a77
Shah Abdullah <smangeles4509@gmail.com>
2025-10-05 17:42:50 -0400

    changes

--- src/controllers/example.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-example = APIRouter(prefix="/api/v1")
-
-
-@example.get("/example")
-async def read_example():
-    return {"message": "hello"}
--- src/controllers/health.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-health = APIRouter(prefix="/api")
-
-
-@health.get("/health")
-async def read_health():
-    return {"message": "service is healthy"}
